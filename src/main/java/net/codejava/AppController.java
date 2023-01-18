6e502ed1827ed4a6875ee3ef2b4ed3cb6201b97e
JesusSN <chuyneri4@gmail.com>
2023-01-18 12:10:00 -0600

    se realiza configuracion

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,7 +14,7 @@ public class AppController {
 	@Autowired
 	private ProductService service; 
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String viewHomePage(Model model) {
 		List<Product> listProducts = service.listAll();
 		model.addAttribute("listProducts", listProducts);
@@ -25,7 +22,7 @@ public class AppController {
 		return "index";
 	}
 	
-	@RequestMapping("/new")
+	@GetMapping("/new")
 	public String showNewProductPage(Model model) {
 		Product product = new Product();
 		model.addAttribute("product", product);
